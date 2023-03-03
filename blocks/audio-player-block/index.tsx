633d5f0af8846a86717ca02741544bd1595b37da
Plamen Totev <plamen.iv.totev@gmail.com>
2023-03-03 22:04:15 +0200

    Fix audio player not playing audio files
    The URL was worng - using only the file name and not the full path.

--- blocks/audio-player-block/index.tsx
+++ blocks/audio-player-block/index.tsx
@@ -4,7 +4,7 @@ import { getURL } from "../utils/url";
 export default function (props: FileBlockProps) {
   const { context } = props;
 
-  const audioURL = getURL(context, context.file);
+  const audioURL = getURL(context, context.path);
 
   return <audio controls src={audioURL}></audio>;
 }
